34738da885591251240e006b099173d18767967a
Winkesa <winfreywekesa148@gmail.com>
2026-07-24 15:04:45 +0300

    Add Assignments component with assignment listing

--- Pages/Mentor/Assignments.jsx
+++ Pages/Mentor/Assignments.jsx
@@ -1,1 +1,45 @@
+import { Link } from "react-router-dom";
 
+function Assignments() {
+    const assignments = [
+        {
+            id:1,
+            title: "Read on materials for sketching",
+            course: "Sketching Classes✏️ ✍️",
+            dueDate: "2026-07-30"
+        }
+    ];
+
+    return (
+        <div>
+            <h1>Assignments</h1>
+
+            <Link to="/assigns">
+            <button>Create Assignments</button>
+            </Link>
+
+            <table>
+
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Course</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {assignments.map((assignment) => {
+                        <tr>
+                            <td>{assignment.title}</td>
+                            <td>{assignment.course}</td>
+                            <td>{assignment.dueDate}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default Assignments;

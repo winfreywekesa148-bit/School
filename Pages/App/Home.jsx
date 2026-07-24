3ea27bfb4a398cda108294cff1e3861cf0186993
Winkesa <winfreywekesa148@gmail.com>
2026-07-24 15:02:28 +0300

    Add Home component with navigation and footer

---
+++ Pages/App/Home.jsx
@@ -0,0 +1,36 @@
+import Footer from "../Components/Footer";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+        <h1>Lupine Art Academy</h1>
+      <h3>Welcome to Lupine Art Academy</h3>
+
+      <p>Our Motto:
+        Art🖼️ never loses its colour🎨, if you love❤️ it
+      </p>
+
+      <div id="about">
+        <Link to="/about">Learn More</Link>
+      </div>
+
+      <div id="login">
+        <Link to="/login">
+        <button>Login</button>
+        </Link>
+      </div>
+
+      <div id="register">
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+
+      <Footer></Footer>
+
+    </div>
+  );
+}
+
+export default Home;

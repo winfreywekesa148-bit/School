b1129868a82396dbed01b22de2f800a3aa51967c
Winkesa <winfreywekesa148@gmail.com>
2026-07-24 14:56:14 +0300

    Implement Navbar component with navigation links

--- Components/Mentor/Navbar.jsx
+++ Components/Mentor/Navbar.jsx
@@ -1,1 +1,22 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a className="nav-brand" href="#top">
+        <span>LUPINE ART ACADEMY</span>
+        TOUCH THE ART
+      </a>
 
+      <div className="nav-links">
+        <ul>
+            <li><a href="#db">Dashboard</a></li>
+            <li><a href="#Lesson">Lesson</a></li>
+            <li><a href="#checkedassign">CheckAssignments</a></li>
+            <li><a href="#graded">Graded</a></li>      
+        </ul>
+        
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

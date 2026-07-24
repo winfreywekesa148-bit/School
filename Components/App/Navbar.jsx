029ff8ad6eb89421993e683f3888e9b74eaf6790
Winkesa <winfreywekesa148@gmail.com>
2026-07-24 14:52:18 +0300

    Add Navbar component for site navigation

---
+++ Components/App/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <a className="nav-brand" href="#top">
+        <span>LUPINE ART ACADEMY</span>
+        TOUCH THE ART
+      </a>
+
+      <div className="nav-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#course">Course</a>
+        <a href="#tasks">Students</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;

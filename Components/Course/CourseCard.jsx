48bb9f1b5d34c3c623227340f5387e41a1a2d801
Winkesa <winfreywekesa148@gmail.com>
2026-07-24 14:54:57 +0300

    Add Delete component to CourseCard

--- Components/Course/CourseCard.jsx
+++ Components/Course/CourseCard.jsx
@@ -1,1 +1,17 @@
+import Delete from "../../App/Components/Delete";
 
+function CourseCard({course, course_id, duration, mentors, onDelete}) {
+    if (!course) return null;
+
+    return (
+        <div className="coursecard">
+            <h3>{course}</h3>
+            <samp>{course_id}</samp>
+            <span>{duration}</span>
+            <p>{mentors}</p>
+            <Delete courses={course} onDelete={onDelete}/>
+        </div>
+
+    )
+}
+export default CourseCard;

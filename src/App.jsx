cc08f0b8d43935a0458fa8c73e8bdc7b77f8e63f
Winkesa <winfreywekesa148@gmail.com>
2026-07-29 14:43:16 +0300

    Routes for the links

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,13 @@ import Home from "./Pages/App/Home";
 import About from "./Pages/App/About";
 import Register from "./Pages/App/Register";
 import Login from "./Pages/App/Login";
-import ForgotPassward from "./Pages/App/ForgotPassword";
-import ResetPassward from "./Pages/App/ResetPassword";
-
-//course
-import Course from "./Pages/Course/Course";
+import ForgotPassword from "./Pages/App/ForgotPassword";
+import ResetPassword from "./Pages/App/ResetPassword";
 
 //mentor
-import Assignmnents from "./Pages/Mentor/Assignments";
-import CreateAssignments from "./Pages/Mentor/CreateAssignment";
-import GradedAssignments from "./Pages/Mentor/GradedAssignments";
-import CreateLessonPlan from "./Pages/Mentor/LessonPlanCreation";
-import LessonPlan from "./Pages/Mentor/Lesson";
 import MtDashboard from "./Pages/Mentor/MtDashboard";
 
 //students
-import Assignms from "./Pages/Student/Assignments";
-import Grades from "./Pages/Student/Grades";
 import StdDashboard from "./Pages/Student/StdDashboard";
 import ProtectedRoute from "./Pages/App/ProtectedRoutes";
 
@@ -36,28 +26,28 @@ function App() {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Home/>}>
-              <Route path="about" element={<About/>}></Route>
-              <Route path="register" element={<Register/>}></Route>
-              <Route path="login" element={<Login/>}></Route>
-              <Route path="forgotpassward" element={<ForgotPassward/>}></Route>
-              <Route path="resetpassword" element={<ResetPassward/>}></Route>
-              <Route path="searchbar" element={<Searchbar/>}></Route>
-            </Route>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/about" element={<About/>}></Route>
+            <Route path="/register" element={<Register/>}></Route>
+            <Route path="/login" element={<Login/>}></Route>
+            <Route path="/forgotpassword" element={<ForgotPassword/>}></Route>
+            <Route path="/resetpassword" element={<ResetPassword/>}></Route>
+            <Route path="/searchbar" element={<Searchbar/>}></Route>
+            
 
-            <Route path="/MtDashboard" element={<ProtectedRoute allowedRole="teacher"><MtDashboard/></ProtectedRoute>}>
-              <Route path="assignments" element={<Assignmnents/>}></Route>
-              <Route path="createassignments" element={<CreateAssignments/>}></Route>
-              <Route path="gradedassignments" element={<GradedAssignments/>}></Route>
-              <Route path="createlessonplan" element={<CreateLessonPlan/>}></Route>
-              <Route path="lessonplan" element={<LessonPlan/>}></Route>
+            <Route path="/MtDashboard" 
+            element={
+            <ProtectedRoute allowedRole="teacher">
+              <MtDashboard/>
+            </ProtectedRoute>}>             
             </Route>
             
-            <Route path="/StdDashboard" element={<ProtectedRoute allowedRole="student"><StdDashboard/></ProtectedRoute>}>
-               <Route path="assignms" element={<Assignms/>}/>
-               <Route path="grades" element={<Grades/>}/>
-               <Route path="course" element={<Course/>}/>
+            <Route path="/StdDashboard" 
+            element={
+            <ProtectedRoute allowedRole="student">
+              <StdDashboard/></ProtectedRoute>}>
             </Route>
+
         </Routes>
         </BrowserRouter>
     )

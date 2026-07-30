16c30a496f539cca170d62c9aa67c5015e692535
Winkesa <winfreywekesa148@gmail.com>
2026-07-30 11:39:10 +0300

    Refactor ProtectedRoute to ProtectedRoutes component

--- Pages/App/ProtectedRoutes.jsx
+++ Pages/App/ProtectedRoutes.jsx
@@ -1,17 +1,18 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-function ProtectedRoute({ children, allowedRole }) {
-    const token = localStorage.getItem("token");
-    const role = localStorage.getItem("role");
+function ProtectedRoutes({ allowedRole }) {
+  const token = localStorage.getItem("access_token");
+  const role = localStorage.getItem("role");
 
-    if (!token) {
-        return <Navigate to="/login" replace />;
-    }
+  if (!token) {
+    return <Navigate to="/Login" replace />;
+  }
 
-    if (allowedRole && role !== allowedRole) {
-        return <Navigate to="/" replace/>;
-    }
+  if (role !== allowedRole) {
+    return <Navigate to="/unauthorized" replace />;
+  }
 
-    return children;
+  return <Outlet />;
 }
-export default ProtectedRoute;
+
+export default ProtectedRoutes;

6971c1eaae50d819ac5ba0c4b42d26110d3510c7
Winkesa <winfreywekesa148@gmail.com>
2026-07-30 12:51:21 +0300

    Change login API endpoint to production URL
    Updated the login API endpoint to use the production URL.

--- Pages/App/Login.jsx
+++ Pages/App/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
     const userInfo = { name, email, password };
 
-    fetch("http://127.0.0.1:5000/login", {
+    fetch("https://school-backend.onrender.com/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
